76713f7c63758b670ac7b40d8c8856d975fcc31c
Kaidi Zhang <kdzhang@gatech.edu>
2018-11-23 13:07:21 -0500

    add GeoRecommendation

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -7,6 +7,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Item {
+
 	private String itemId;
 	private String name;
 	private double rating;
@@ -118,6 +119,31 @@ public class Item {
 			e.printStackTrace();
 		}
 		return obj;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		return true;
 	}
 
 	public static class ItemBuilder {
